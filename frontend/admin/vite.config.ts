43396b47bb6af218fb22c4878184d3a054bd5af3
Guanyu Hu <ikaroinory@gmail.com>
2026-05-28 20:05:44 +0800

    chore: 删除旧的scss文件

--- frontend/admin/vite.config.ts
+++ frontend/admin/vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import semiTheming from '@douyinfe/semi-vite-plugin';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    semiTheming({
+      theme: '@semi-bot/semi-theme-doucreator'
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
